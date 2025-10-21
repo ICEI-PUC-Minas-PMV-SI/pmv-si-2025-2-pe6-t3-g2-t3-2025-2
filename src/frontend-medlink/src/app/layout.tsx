9807ea8b1659fa90515f9a34e43da21d0712a70d
Andre Fabiano <andregtalima@gmail.com>
2025-10-20 21:38:14 -0300

    Iniciando projeto Next.js na pasta src/frontend-medlink

---
+++ src/frontend-medlink/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google"
+import "./globals.css"
+
+export const metadata: Metadata = {
+  title: "Medlink",
+};
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter'
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR" className={inter.className}>
+      <body>{children}</body>
+    </html>
+  );
+}
