9b8542385896a2f7fa77ea7c71972bdf10b14179
Andre Fabiano <andregtalima@gmail.com>
2025-10-20 22:59:45 -0300

    Criando componentsda navbar e do botão de agendamento

--- src/frontend-medlink/src/app/components/navbar/navbar.tsx
+++ src/frontend-medlink/src/app/components/navbar/navbar.tsx
@@ -1,17 +1,35 @@
-import { ComponentProps } from "react";
 import "./styles.css"
 import logo from "../../assets/logo.svg"
 import logo_name from "../../assets/logo_nome.svg"
 import Image from "next/image";
+import Link from "next/link";
 
-interface NavbarProps extends ComponentProps<'link'> {}
+interface NavbarLink {
+    label: string
+    href: string
+}
 
-export function Navbar(props: NavbarProps) {
+export function Navbar() {
+
+    const links: NavbarLink[] = [
+        {label: "Sobre", href: "#sobre"},
+        {label: "Especialidades", href: "#especialidades"},
+        {label: "Profissionais", href: "#profissionais"},
+        {label: "Entrar", href: "#entrar"},
+    ]
+
     return (
         <nav>
-            <div className="logo">
-               <Image src={logo} alt="logo Medlink" width={36}></Image>
-               <Image src={logo_name} alt="logo Medlink" width={120}></Image>
+            <Link href="/" className="logo">
+                <Image src={logo} alt="logo Medlink" width={36}></Image>
+                <Image src={logo_name} alt="logo Medlink" width={120}></Image>
+            </Link>
+            <div className="links">
+                {links.map((link) => (
+                    <Link href={link.href} key={link.label} className="link">
+                        {link.label}
+                    </Link>
+                ))}
             </div>
         </nav>
     )
