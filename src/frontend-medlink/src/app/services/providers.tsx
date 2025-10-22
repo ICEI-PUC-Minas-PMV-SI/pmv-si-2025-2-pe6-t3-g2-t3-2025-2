0681a605e6412c502ed41935dfc3de0bba50f06b
Andre Fabiano <andregtalima@gmail.com>
2025-10-22 13:28:50 -0300

    configurando axios e tanstack-query

--- src/frontend-medlink/src/app/services/providers.tsx
+++ src/frontend-medlink/src/app/services/providers.tsx
@@ -2,18 +2,18 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-export const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            refetchOnWindowFocus: false,
-            retry: 1,
-            staleTime: 1000 * 60,
-        }
-    }
-})
+import { useState } from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+    const [queryClient] = useState(() => new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: false,
+                retry: 1,
+                staleTime: 1000 * 60
+            }
+        }
+    }))
     return (
         <QueryClientProvider client={queryClient}>
             {children}
