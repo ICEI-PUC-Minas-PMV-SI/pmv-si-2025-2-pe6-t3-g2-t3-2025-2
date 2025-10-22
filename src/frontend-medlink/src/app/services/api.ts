4e74e5244b1802b70c8005b47fa72b97f0060b14
Andre Fabiano <andregtalima@gmail.com>
2025-10-22 13:28:50 -0300

    configurando axios e tanstack-query

--- src/frontend-medlink/src/app/services/api.ts
+++ src/frontend-medlink/src/app/services/api.ts
@@ -9,19 +9,23 @@ if (!baseUrl) {
 export const api = axios.create({
     baseURL: baseUrl,
     timeout: 10000,
-    headers: {
-        "Content-Type": "application/json"
-    }
 })
 
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("token");
+api.interceptors.request.use((config) => {
+    if (typeof window !== "undefined") {
+        const token = localStorage.getItem("token")
         if (token) {
             config.headers.Authorization = `Bearer ${token}`
         }
+    }
+    return config
+})
 
-        return config
-    },
-    (error) => Promise.reject(error)
+api.interceptors.response.use((response) => response, (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem("token")
+            window.location.href = "/login"
+        }
+        return Promise.reject(error)
+    }
 )
--- src/frontend-medlink/src/app/services/auth.ts
+++ src/frontend-medlink/src/app/services/auth.ts
@@ -1,23 +1,48 @@
-import { api } from "./api";
+"use client"
 
-export interface RegisterData {
+import { api } from "./api"
+import { useMutation } from "@tanstack/react-query"
+import { useRouter } from "next/navigation"
+
+interface RegisterData {
     name: string
     email: string
     phone: string
     password: string
+    address?: string
 }
 
-export interface RegisterResponse {
-    token?: string
-    message?: string
+interface LoginData {
+    email: string
+    password: string
 }
 
-export async function registerUser(data: RegisterData): Promise<RegisterResponse> {
-    const response = await api.post("/medlink/register", data)
+export const useRegister = () => {
+    const router = useRouter()
 
-    if (response.data.token) {
-        localStorage.setItem("token", response.data.token)
-    }
+    return useMutation({
+        mutationFn: async (data: RegisterData) => {
+            const response = await api.post("/medlink/register", data)
+            return response.data
+        },
+        onSuccess: (data) => {
+            localStorage.setItem("token", data.token)
+            console.log(data)
+        }
+    })
+}
 
-    return response.data
+export const useLogin = () => {
+    const router = useRouter()
+
+    return useMutation({
+        mutationFn: async (data: LoginData) => {
+            const response = await api.post("/medlink/login", data)
+            return response.data         
+        },
+        onSuccess: (data) => {
+            localStorage.setItem("token", data.token)
+            router.push("/scheduling")
+        }
+    })
 }
